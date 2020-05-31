b80ce8301e583edda126cb80943db8602bcdc73f
Wechuli <wechulipaul@yahoo.com>
2020-05-31 10:37:26 +0300

    index.js with setup server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+// middleware
+app.use(morgan("common"));
+app.use(cors());
+app.use(helmet());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
